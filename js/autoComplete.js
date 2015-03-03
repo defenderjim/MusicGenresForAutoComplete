e16680c89847b3831db66378ea1f2cad02048601
defenderjim <defenderjim@yahoo.com>
2015-03-03 16:33:11 -0500

    Auto Complete for music genres
    Based on DevBridges' Autocomplete.
    List of genres from www.musicgenreslist.com

---
+++ js/autoComplete.js
@@ -0,0 +1,78 @@
+﻿/*jslint  browser: true, white: true, plusplus: true */
+/*global $, countries */
+
+$(function () {
+    'use strict';
+
+    var genresArray = $.map(genres, function (value, key) { return { value: value, data: key }; });
+
+/*
+    // Setup jQuery ajax mock:
+    $.mockjax({
+        url: '*',
+        responseTime: 2000,
+        response: function (settings) {
+            var query = settings.data.query,
+                queryLowerCase = query.toLowerCase(),
+                re = new RegExp('\\b' + $.Autocomplete.utils.escapeRegExChars(queryLowerCase), 'gi'),
+                suggestions = $.grep(countriesArray, function (country) {
+                     // return country.value.toLowerCase().indexOf(queryLowerCase) === 0;
+                    return re.test(country.value);
+                }),
+                response = {
+                    query: query,
+                    suggestions: suggestions
+                };
+
+            this.responseText = JSON.stringify(response);
+        }
+    });
+
+
+    // Initialize ajax autocomplete:
+    $('#autocomplete-ajax').autocomplete({
+        // serviceUrl: '/autosuggest/service/url',
+        lookup: genresArray,
+        lookupFilter: function(suggestion, originalQuery, queryLowerCase) {
+            var re = new RegExp('\\b' + $.Autocomplete.utils.escapeRegExChars(queryLowerCase), 'gi');
+            return re.test(suggestion.value);
+        },
+        onSelect: function(suggestion) {
+            $('#selction-ajax').html('You selected: ' + suggestion.value + ', ' + suggestion.data);
+        },
+        onHint: function (hint) {
+            $('#autocomplete-ajax-x').val(hint);
+        },
+        onInvalidateSelection: function() {
+            $('#selction-ajax').html('You selected: none');
+        }
+    });
+*/
+
+    // Initialize autocomplete with local lookup:
+     // Your Code
+
+    $('.autocomplete').devbridgeAutocomplete({
+        lookup: genres,
+        minChars: 1,
+        onSelect: function (suggestion) {
+            $('.selection').html('You selected: ' + suggestion.value + ', ' + suggestion.data.category);
+        },
+        showNoSuggestionNotice: true,
+        noSuggestionNotice: 'Sorry, no matching results',
+        groupBy: 'category'
+    }) ;
+    
+    // Initialize autocomplete with custom appendTo:
+    $('#autocomplete-custom-append').autocomplete({
+        lookup: genresArray,
+        appendTo: '#suggestions-container'
+    });
+
+    // Initialize autocomplete with custom appendTo:
+    $('#autocomplete-dynamic').autocomplete({
+        lookup: genresArray
+    });
+
+
+});
---
+++ js/genres.js
@@ -0,0 +1,404 @@
+// http://www.musicgenreslist.com/music-genre-list-complete-list-music-styles-types-genres.pdf
+// Please note: This is a free PDF download to use for you own personal or business use.
+// Though not required, it would be appreciated if you could link back to the
+// Music Genres List site so that others may find and add to this resource
+// www.musicgenreslist.com
+
+
+
+var alternative = [
+	'Alternative',
+	'Alternative Rock',
+	'College Rock',
+	'Experimental Rock',
+	'Goth Rock',
+	'Grunge',
+	'Hardcore Punk',
+	'Hard Rock',
+	'Indie Rock',
+	'New Wave',
+	'Progressive Rock',
+	'Punk',
+	'Shoegaze',
+	'Steampunk'
+];
+
+var anime = [
+	'Anime'
+];
+
+var blues = [
+	'Blues',
+	'Acoustic Blues',
+	'Chicago Blues',
+	'Classic Blues',
+	'Contemporary Blues',
+	'Country Blues',
+	'Delta Blues',
+	'Electric Blues'
+];
+
+var childrens = [
+	'Children’s Music',
+	'Lullabies',
+	'Sing Along',
+	'Stories'
+];
+
+var classical = [
+	'Classical',
+	'Avant-Garde',
+	'Baroque',
+	'Chamber Music',
+	'Chant',
+	'Choral',
+	'Classical Crossover',
+	'Early Music',
+	'High Classical',
+	'Impressionist',
+	'Medieval',
+	'Minimalism',
+	'Modern Composition',
+	'Opera',
+	'Orchestral',
+	'Renaissance',
+	'Romantic',
+	'Wedding Music'
+];
+
+var comedy = [
+	'Comedy',
+	'Novelty',
+	'Standup Comedy'
+];
+
+var country = [
+	'Country',
+	'Alternative Country',
+	'Americana',
+	'Bluegrass',
+	'Contemporary Bluegrass',
+	'Contemporary Country',
+	'Country Gospel',
+	'Honky Tonk',
+	'Outlaw Country',
+	'Traditional Bluegrass',
+	'Traditional Country',
+	'Urban Cowboy'
+];
+
+var dance = [
+	'Dance',
+	'Breakbeat',
+	'Dubstep',
+	'Exercise',
+	'Garage',
+	'Hardcore',
+	'Hard Dance',
+	'HiNRG / Eurodance',
+	'House',
+	'Jackin House',
+	'Jungle/Drum & bass',
+	'Techno',
+	'Trance'
+];
+
+var disney = [
+	'Disney'
+];
+
+var easyListening = [
+	'Easy Listening',
+	'Bop',
+	'Lounge',
+	'Swing'
+];
+
+var electronic = [
+	'Electronic',
+	'Ambient',
+	'Crunk',
+	'Downtempo',
+	'Electro',
+	'Electronica',
+	'Electronic Rock',
+	'IDM/Experimental',
+	'Industrial'
+];
+
+var enka = [
+	'Enka'
+];
+
+var french = [
+	'French Pop'
+];
+
+var german = [
+	'German Folk',
+	'German Pop'
+];
+
+
+var fitness = [
+	'Fitness & Workout'
+];
+
+var hipHop = [
+	'Hip Hop/Rap',
+	'Alternative Rap',
+	'Bounce',
+	'Dirty South',
+	'East Coast Rap',
+	'Gangsta Rap',
+	'Hardcore Rap',
+	'Hip',
+	'Hop',
+	'Latin Rap',
+	'Old School Rap',
+	'Rap',
+	'Underground Rap',
+	'West Coast Rap'
+];
+
+var holiday = [
+	'Holiday',
+	'Chanukah',
+	'Christmas',
+	'Christmas: Children’s',
+	'Christmas: Classic',
+	'Christmas: Classical',
+	'Christmas: Jazz',
+	'Christmas: Modern',
+	'Christmas: Pop',
+	'Christmas: R&B',
+	'Christmas: Religious',
+	'Christmas: Rock',
+	'Easter',
+	'Halloween',
+	'Holiday: Other',
+	'Thanksgiving'
+];
+
+var indie = [
+	'Indie Pop'
+];
+
+var industrial = [
+	'Industrial'
+];
+
+var kpop = [
+	'K-Pop'
+];
+
+var karaoke = [
+	'Karaoke'
+];
+
+var kayo = [
+	'Kayokyoku'
+];
+
+var latino = [
+	'Latino',
+	'Alternativo & Rock Latino',
+	'Baladas y Boleros',
+	'Brazilian',
+	'Contemporary Latin',
+	'Latin Jazz',
+	'Pop Latino',
+	'Raíces',
+	'Reggaeton y HipHop',
+	'Regional Mexicano',
+	'Salsa y Tropical'
+];
+
+var newAge = [
+	'New Age',
+	'Environmental',
+	'Healing',
+	'Meditation',
+	'Nature',
+	'Relaxation',
+	'Travel'
+];
+
+var opera = [
+	'Opera'
+];
+
+var pop = [
+	'Pop',
+	'Adult Contemporary',
+	'Britpop',
+	'Pop/Rock',
+	'Soft Rock',
+	'Teen Pop'
+];
+
+var rnb = [
+	'R&B/Soul',
+	'Contemporary R&B',
+	'Disco',
+	'Doo Wop',
+	'Funk',
+	'Motown',
+	'Neo Soul',
+	'Quiet Storm',
+	'Soul'
+];
+
+var reggae = [
+	'Reggae',
+	'Dancehall',
+	'Dub',
+	'Roots Reggae',
+	'Ska'
+];
+
+var rock = [
+	'Rock',
+	'Adult Alternative',
+	'American Trad Rock',
+	'Arena Rock',
+	'Blues-Rock',
+	'British Invasion',
+	'Death Metal/Black Metal',
+	'Glam Rock',
+	'Hair Metal',
+	'Hard Rock',
+	'Metal',
+	'Jam Bands',
+	'Prog-Rock/Art Rock',
+	'Psychedelic',
+	'Rock & Roll',
+	'Rockabilly',
+	'Roots Rock',
+	'Singer/Songwriter',
+	'Southern Rock',
+	'Surf',
+	'Tex-Mex'
+];
+
+var singer = [
+	'Singer/Songwriter',
+	'Alternative Folk',
+	'Contemporary Folk',
+	'Contemporary Singer/Songwriter',
+	'Folk-Rock',
+	'New Acoustic',
+	'Traditional Folk'
+];
+
+var soundTrack = [
+	'Soundtrack',
+	'Foreign Cinema',
+	'Musicals',
+	'Original Score',
+	'Soundtrack',
+	'TV Soundtrack'
+];
+
+var spoken = [
+	'Spoken Word',
+];
+
+var texMex = [
+	'Tex-Mex / Tejano',
+	'Chicano',
+	'Classic',
+	'Conjunto',
+	'Conjunto Progressive',
+	'New Mex',
+	'Tex-Mex'
+];
+
+var vocal = [
+	'Vocal',
+	'Barbershop',
+	'Doo-wop',
+	'Standards',
+	'Traditional Pop',
+	'Vocal Jazz',
+	'Vocal Pop'
+];
+
+var world = [
+	'World',
+	'Africa',
+	'Afro-Beat',
+	'Afro-Pop',
+	'Asia',
+	'Australia',
+	'Bossa Nova',
+	'Cajun',
+	'Caribbean',
+	'Celtic',
+	'Celtic Folk',
+	'Contemporary Celtic',
+	'Drinking Songs',
+	'Drone',
+	'Europe',
+	'France',
+	'Hawaii',
+	'Indian Pop',
+	'Japan',
+	'Japanese Pop',
+	'Klezmer',
+	'Middle East',
+	'North America',
+	'Polka',
+	'South Africa',
+	'South America',
+	'Traditional Celtic',
+	'Worldbeat',
+	'Zydeco'
+];
+
+
+var alternativMap = $.map(alternative  , function (team) { return { value: team, data: { category: 'Alternative'         }}; });
+var animeMap      = $.map(anime        , function (team) { return { value: team, data: { category: 'Anime'               }}; });
+var bluesMap      = $.map(blues        , function (team) { return { value: team, data: { category: 'Blues'               }}; });
+var childrensMap  = $.map(childrens    , function (team) { return { value: team, data: { category: 'Childrens'           }}; });
+var classicalMap  = $.map(classical    , function (team) { return { value: team, data: { category: 'Classical'           }}; });
+var comedyMap     = $.map(comedy       , function (team) { return { value: team, data: { category: 'Comedy'              }}; });
+var countryMap    = $.map(country      , function (team) { return { value: team, data: { category: 'Country'             }}; });
+var danceMap      = $.map(dance        , function (team) { return { value: team, data: { category: 'Dance'               }}; });
+var disneyMap     = $.map(disney       , function (team) { return { value: team, data: { category: 'Disney'              }}; });
+var easyListenMap = $.map(easyListening, function (team) { return { value: team, data: { category: 'Easy Listening'      }}; });
+var electronicMap = $.map(electronic   , function (team) { return { value: team, data: { category: 'Electronic'          }}; });
+var enkaMap       = $.map(enka         , function (team) { return { value: team, data: { category: 'Enka'                }}; });
+var frenchMap     = $.map(french       , function (team) { return { value: team, data: { category: 'French'              }}; });
+var germanMap     = $.map(german       , function (team) { return { value: team, data: { category: 'German'              }}; });
+var fitnessMap    = $.map(fitness      , function (team) { return { value: team, data: { category: 'Fitness / Workout'   }}; });
+var hipHopMap     = $.map(hipHop       , function (team) { return { value: team, data: { category: 'Hip Hop'             }}; });
+var holidayMap    = $.map(holiday      , function (team) { return { value: team, data: { category: 'Holiday'             }}; });
+var indieMap      = $.map(indie        , function (team) { return { value: team, data: { category: 'Indie'               }}; });
+var industrialMap = $.map(industrial   , function (team) { return { value: team, data: { category: 'Industrial'          }}; });
+var kpopMap       = $.map(kpop         , function (team) { return { value: team, data: { category: 'K-Pop'               }}; });
+var karaokeMap    = $.map(karaoke      , function (team) { return { value: team, data: { category: 'Karaoke'             }}; });
+var kayoMap       = $.map(kayo         , function (team) { return { value: team, data: { category: 'Kayo'                }}; });
+var latinoMap     = $.map(latino       , function (team) { return { value: team, data: { category: 'Latino'              }}; });
+var newAgeMap     = $.map(newAge       , function (team) { return { value: team, data: { category: 'New Age'             }}; });
+var operaMap      = $.map(opera        , function (team) { return { value: team, data: { category: 'Opera'               }}; });
+var popMap        = $.map(pop          , function (team) { return { value: team, data: { category: 'Pop'                 }}; });
+var rnbMap        = $.map(rnb          , function (team) { return { value: team, data: { category: 'R & B'               }}; });
+var reggaeMap     = $.map(reggae       , function (team) { return { value: team, data: { category: 'Reggae'              }}; });
+var rockMap       = $.map(rock         , function (team) { return { value: team, data: { category: 'Rock'                }}; });
+var singerMap     = $.map(singer       , function (team) { return { value: team, data: { category: 'Singer / Songwriter' }}; });
+var soundTrkMap   = $.map(soundTrack   , function (team) { return { value: team, data: { category: 'Soundtrack'          }}; });
+var spokenMap     = $.map(spoken       , function (team) { return { value: team, data: { category: 'Spoken Word'         }}; });
+var texMexMap     = $.map(texMex       , function (team) { return { value: team, data: { category: 'Tex-Mex'			 }}; });
+var vocalMap      = $.map(vocal        , function (team) { return { value: team, data: { category: 'Vocal'               }}; });
+var worldMap      = $.map(world        , function (team) { return { value: team, data: { category: 'World'               }}; });
+
+
+var genres = alternativMap.concat(
+	animeMap, bluesMap, childrensMap, classicalMap, comedyMap, countryMap, danceMap, disneyMap, easyListenMap,
+	electronicMap, enkaMap, frenchMap, germanMap, fitnessMap, hipHopMap, holidayMap, indieMap, industrialMap,
+	kpopMap, karaokeMap, kayoMap, latinoMap, newAgeMap, operaMap, popMap, rnbMap, reggaeMap, rockMap, singerMap,
+	soundTrkMap, spokenMap, texMexMap, vocalMap, worldMap
+);
+
+
